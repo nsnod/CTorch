cca185cd604a853dd6551df178d9c70a67967de2
nsnod001 <nsnod001@ucr.edu>
2024-11-03 14:32:43 -0800

    Make and cmake

--- main.cpp
+++ main.cpp
@@ -1,1 +1,8 @@
 // driver code for testing
+#include "linear.h"
+#include "tensor.h"
+#include <iostream>
+
+int main() {
+    return 0;
+}
--- src/linear.h
+++ src/linear.h
@@ -1,23 +1,23 @@
-#pragma once
-#include "linear.h"
-#include "tensor.h"
-#include <iostream>
-#include <vector>
+// #pragma once
+// #include "linear.h"
+// #include "tensor.h"
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-class LinearLayer{
-public:
-    Tensor<float> weights;
+// class LinearLayer{
+// public:
+//     Tensor<float> weights;
 
-    LinearLayer(int input_size, int output_size){
-        weights = Tensor<float>(input_size, output_size); 
-    }
-    ~LinearLayer(){
+//     LinearLayer(int input_size, int output_size){
+//         weights = Tensor<float>(input_size, output_size); 
+//     }
+//     ~LinearLayer(){
 
-    }
+//     }
 
-    void forward(Tensor<float> input){
-        return input.matmul(weights);
-    }
-};
+//     void forward(Tensor<float> input){
+//         return input.matmul(weights);
+//     }
+// };
