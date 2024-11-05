34caf3d620524b0521cf3f589390209e22cd5206
nsnod001 <nsnod001@ucr.edu>
2024-11-04 18:03:46 -0800

    wohoo testing the test module

--- tests/tensorTests.cpp
+++ tests/tensorTests.cpp
@@ -1,7 +1,17 @@
 #include <gtest/gtest.h>
 #include "../src/tensor.h"
 
-
 int test1() {
+    std::cout << "test" << std::endl;
     return 1;
+}
+
+TEST(ExampleTest, OneIsOne) {
+    int result = test1();
+    EXPECT_EQ(result, 1) << "Expected result to be 1, but got " << result;
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
