94fcae43dad9154bf26d1777d793a00fcbd199a2
nsnod001 <nsnod001@ucr.edu>
2024-11-05 17:06:34 -0800

    Separated header file and implementation file

--- src/array.cpp
+++ src/array.cpp
@@ -1,1 +1,33 @@
 #include "array.h"
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void Array<T>::calcStrides(vector<int>& strides, vector<int> shape, int dimension) {
+    strides.resize(dimension);
+    strides[dimension - 1] = 1;
+    for (int i = dimension - 2; i >= 0; --i) {
+        strides[i] = strides[i + 1] * shape[i + 1];
+    }
+}
+
+template <typename T>
+void Array<T>::zeroTensor() {
+    fill(data.begin(), data.end(), 0);
+}
+
+template <typename T>
+int Array<T>::flatIndex(const vector<int>& indices) const {
+    int oneDimIndex = 0;
+    for (int i = 0; i < dimension; ++i) {
+        oneDimIndex += indices[i] * strides[i];
+    }
+    return oneDimIndex;
+}
+
+template <typename T>
+T& Array<T>::at(const vector<int>& indices) {
+    int oneDimIndex = flatIndex(indices);
+    return data[oneDimIndex];
+}
--- src/array.h
+++ src/array.h
@@ -5,64 +5,36 @@
 using namespace std;
 
 template <typename T>
-
-class Array{
-  private:
+class Array {
+private:
     vector<int> shape;
     vector<T> data;
     vector<int> strides;
     int dimension;
     int size;
 
-    void calcStrides() {
-        strides.resize(dimension);
-        strides[dimension - 1] = 1;
-        // - 2 instead of - 1 because the last dimension will always be one move iterable. no skipping needed
-        for (int i = dimension - 2; i >= 0; --i) {
-            strides[i] = strides[i + 1] * shape[i + 1];
-        }
+public:
+    // Setter
+    void setDim(int dim) { dimension = dim; }
+    void setSize(int s) { size = s; }
+
+    // Constructor
+    Array(vector<T>& data, vector<int>& shape) : data(data), shape(shape) {
+        setDim(shape.size());
+        setSize(data.size());
+        calcStrides(strides, shape, dimension);
     }
 
-    Array(vector<int> shape) : shape(shape) {
-        dimension = shape.size();
-        size = 1;
+    // Destructor
+    ~Array() {}
 
-        for (int dim : shape) {
-            size *= dim;
-        }
-
-        data.resize(size, 0); 
-
-        calcStrides();
-    }
-
-    Array(vector<T> data, vector<int> shape) : data(data), shape(shape) {
-        dimension = shape.size();
-
-        size = data.size();
-
-        calcStrides();
-    }
-
-    ~Array() {
-
-    }
-
-    int flatIndex(const vector<int>& indices) const {
-        //try and catch if the indices is the same as the size of stride
-        int oneDimIndex = 0;
-        for (int i = 0; i < dimension; ++i) {
-            index += indices[i] * strides[i];
-        }
-        return oneDimIndex;
-    }
-
-    //getter
-    T& at(const vector<int>& indices) {
-        int oneDimIndex = flatIndex(indices);
-        return data[oneDimIndex];
-    }
-
+    // Functions
+    void zeroTensor();
+    void calcStrides(vector<int>& strides, vector<int> shape, int dimension);
+    int flatIndex(const vector<int>& indices) const;
+    
+    // Getter
+    T& at(const vector<int>& indices);
 
     //transpose functionality (inverting shape)
     //create an array of random ints? maybe could be handled in tensor then passed to array?
