7ce1f373cbfabd991aceba0b3ccfdad4481c8ceb
nsnod001 <nsnod001@ucr.edu>
2024-11-06 22:48:34 -0800

    Array class complete. Still need to add transpose function. Tested a 3,3 matrix and it works

--- src/array.h
+++ src/array.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include <iostream>
+#include <random>
 
 using namespace std;
 
@@ -20,9 +22,18 @@ public:
     void setData(int index, T value) { data[index] = value; }
 
     // Constructor
-    Array(vector<int>& shape) : shape(shape) {
+    Array(const vector<int>& shape) : shape(shape) {
         setDim(shape.size());
-        setSize(data.size());
+        int size = 1;
+        for (int i = 0; i < getDim(); i++) {
+            size *= shape[i];
+        }
+        setSize(size);
+        data.resize(getSize());
+        for (int i = 0; i < size; i++) {
+            setData(i, 0);
+        }
+
         calcStrides(strides, shape, dimension);
     }
 
@@ -40,11 +51,83 @@ public:
     int getSize() { return size; }
     vector<int> getShape() { return shape; }
     vector<int> getStrides() { return strides; }
-    vector<T> getData() { return data; }
     void print();
 
     T& at(const vector<int>& indices);
 
     //transpose functionality (inverting shape)
-    //create an array of random ints? maybe could be handled in tensor then passed to array?
 };
+
+
+
+
+// FUNCTION IMPLEMENTATIONS HAVE TO STAY HERE BECAUSE OF TEMPLATE 
+template <typename T>
+void Array<T>::calcStrides(vector<int>& strides, vector<int> shape, int dimension) {
+    strides.resize(dimension);
+    strides[dimension - 1] = 1;
+    for (int i = dimension - 2; i >= 0; i--) {
+        strides[i] = strides[i + 1] * shape[i + 1];
+    }
+}
+
+template <typename T>
+void Array<T>::randomize(float upper, float lower) {
+    random_device rd; 
+    mt19937 gen(rd()); 
+    uniform_real_distribution<float> dist(lower, upper); 
+    for (int i = 0; i < getSize(); i++) {
+        setData(i, dist(gen));
+    }
+}
+
+// work on
+template <typename T>
+void Array<T>::print() {
+    vector<int> indices(getDim(), 0);
+
+    for (int i = 0; i < getSize(); i++) {
+        // bracket printing
+        for (int d = 0; d < getDim(); d++) {
+            if (indices[d] == 0) {
+                cout << "[";
+            }
+        }
+
+        cout << at(indices);
+
+        // Update indices to traverse to the next element
+        for (int d = getDim() - 1; d >= 0; d--) {
+            indices[d]++;
+            if (indices[d] < shape[d]) {
+                break;
+            }
+            indices[d] = 0; 
+            cout << "]";    
+        }
+
+        if (i < getSize() - 1) {
+            cout << ", ";
+        }
+    }
+
+    for (int d = 0; d < getDim(); d++) {
+        cout << "]";
+    }
+    cout << endl;
+}
+
+template <typename T>
+int Array<T>::flatIndex(const vector<int> indices) const {
+    int oneDimIndex = 0;
+    for (int i = 0; i < dimension; i++) {
+        oneDimIndex += indices[i] * strides[i];
+    }
+    return oneDimIndex;
+}
+
+template <typename T>
+T& Array<T>::at(const vector<int>& indices) {
+    int oneDimIndex = flatIndex(indices);
+    return data[oneDimIndex];
+}
--- tests/arrayTests.cpp
+++ tests/arrayTests.cpp
@@ -1,12 +1,23 @@
 #include <gtest/gtest.h>
 #include "../src/array.h"
 
-TEST(ExampleTest, OneIsOne) {
-    int result = 1;
-    EXPECT_EQ(result, 1) << "Expected result to be 1, but got " << result;
-}
+using namespace std;
+
+// TEST(ExampleTest, OneIsOne) {
+//     int result = 1;
+//     EXPECT_EQ(result, 1) << "Expected result to be 1, but got " << result;
+// }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
+
+    Array<float> myArray({3, 3});
+
+    cout << myArray.getDim() << " " << myArray.getSize() << endl;
+
+    myArray.randomize(-1.0, 2.0);
+
+    myArray.print();
+
     return RUN_ALL_TESTS();
 }
