ca2c49c88fe55a2b9b84e3f476478e437200509d
Arkes123 <jonathan.darius2015@gmail.com>
2024-12-10 01:24:05 -0800

    no I didn't

--- src/tensor.h
+++ src/tensor.h
@@ -294,7 +294,6 @@ class Tensor {
             MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
         }
 
-        // MPI CODE
         int rank, size;
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
         MPI_Comm_size(MPI_COMM_WORLD, &size);
